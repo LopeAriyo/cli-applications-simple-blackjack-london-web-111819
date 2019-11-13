4d3594eee075e9867cd343a8476b8bcc168b4369
Lopè Ariyo <lopeariyo@gmail.com>
2019-11-13 19:26:12 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,84 @@
+require 'pry'
+require 'pp'
+
 def welcome
   # code #welcome here
+  puts("Welcome to the Blackjack Table")
 end
 
 def deal_card
   # code #deal_card here
+
+  card = rand(1..11)
+  card
+
 end
 
-def display_card_total
+def display_card_total(card_total)
   # code #display_card_total here
+
+    puts("Your cards add up to #{card_total}")
+
 end
 
 def prompt_user
   # code #prompt_user here
+
+  puts("Type 'h' to hit or 's' to stay")
+
 end
 
 def get_user_input
   # code #get_user_input here
+  gets.chomp
 end
 
-def end_game
+def end_game (card_total)
   # code #end_game here
+
+  puts("Sorry, you hit #{card_total}. Thanks for playing!")
 end
 
 def initial_round
   # code #initial_round here
+
+  card_one = deal_card
+  card_two = deal_card
+
+  sum = card_one + card_two
+
+  display_card_total(sum)
+  sum
+
 end
 
-def hit?
+def hit? (card_total)
   # code hit? here
+  prompt_user
+  get_user_input
+
+
+  if get_user_input == "h"
+    deal_card
+    #card_total += deal_card
+  end
+
+  if get_user_input != "s" || get_user_input != "h"
+    invalid_command
+    prompt_user
+    get_user_input
+  #binding.pry
+ end
+
+  return card_total
+
+
+
 end
 
 def invalid_command
   # code invalid_command here
+  puts "Please enter a valid command"
 end
 
 #####################################################
@@ -41,4 +88,3 @@ end
 def runner
   # code runner here
 end
-    
